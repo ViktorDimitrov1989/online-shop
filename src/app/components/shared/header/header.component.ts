df8c7d047001f5860c2164aad6d1490a284c2986
ViktorDimitrov1989 <viktor.dimtirov.1989@gmail.com>
2018-03-07 12:07:22 +0200

    added footer/header html+css

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,41 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.initJquery();
+  };
+
+  private initJquery(){
+    $(window).on('scroll', function () {
+      if ($(this).scrollTop() >= 200) {
+        $('.navbar').addClass('fixed-top');
+      } else if ($(this).scrollTop() == 0) {
+        $('.navbar').removeClass('fixed-top');
+      }
+    });
+
+    function adjustNav() {
+      var winWidth = $(window).width(),
+        dropdown = $('.dropdown'),
+        dropdownMenu = $('.dropdown-menu');
+
+      if (winWidth >= 768) {
+        dropdown.on('mouseenter', function () {
+          $(this).addClass('show')
+            .children(dropdownMenu).addClass('show');
+        });
+
+        dropdown.on('mouseleave', function () {
+          $(this).removeClass('show')
+            .children(dropdownMenu).removeClass('show');
+        });
+      } else {
+        dropdown.off('mouseenter mouseleave');
+      }
+    }
+
+    $(window).on('resize', adjustNav);
+
+    adjustNav();
   }
 
 }
