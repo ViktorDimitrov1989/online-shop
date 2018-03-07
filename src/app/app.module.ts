9f92d24b9cc4bc1e581f2c2bf8f5c3c586b2b99f
ViktorDimitrov1989 <viktor.dimtirov.1989@gmail.com>
2018-03-07 10:59:16 +0200

    setup bootstrap/create main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,39 @@ import { NgModule } from '@angular/core';
 import { NgReduxModule, NgRedux } from 'ng2-redux';
 import { store, IArticleState} from './store';
 
+//routing
+import { AppRoutingModule } from './routing/app-routing';
+
+//toastr
+import { ToastrModule } from 'ngx-toastr';
+//bootstrap
+import { AppBootstrapModule } from './modules/app-bootstrap/app-bootstrap.module';
+
+
 import { AppComponent } from './app.component';
 import { ArticleActions } from './store/article/article.actions';
 import { ArticleService } from './services/article/article.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/auth/login/login.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
+    AppBootstrapModule,
     HttpClientModule,
-    NgReduxModule
+    NgReduxModule,
+    AppRoutingModule,
+    ToastrModule.forRoot()
   ],
   providers: [
     ArticleActions,
---
+++ src/app/routing/app-routing.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../components/auth/login/login.component';
+
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    //canActivate: [NotAuthGuard]
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
