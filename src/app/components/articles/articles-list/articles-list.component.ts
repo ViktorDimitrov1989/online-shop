0126447d2298f511a420123e64486c275107f1ad
Viktor <viktor.dimitrov.1989@gmail.com>
2018-04-03 22:33:17 +0300

    working on filtering logic

--- src/app/components/articles/articles-list/articles-list.component.ts
+++ src/app/components/articles/articles-list/articles-list.component.ts
@@ -12,21 +12,22 @@ import { AppState } from '../../../store/app-state';
   styleUrls: ['./articles-list.component.css']
 })
 export class ArticlesListComponent implements OnInit {
-  
+
   public showFiller = false;
   public articlesList: any[];
   public brands: any[];
   public colors: any[];
   public sizes: any[];
   public categories: any[];
-
-  public chosenBrands: any[] = [];
-  public chosenColors: any[] = [];
-  public chosenSizes: any[] = [];
-  public chosenCategories: any[] = [];
-  public chosenStatuses: any[] = [];
-
   public articlesLength: number;
+
+  public forbiddenBrands: any[] = [];
+  public forbiddenColors: any[] = [];
+  public forbiddenSizes: any[] = [];
+  public forbiddenCategories: any[] = [];
+  public forbiddenStatuses: any[] = ['PROMO', 'REGULAR', 'SALE'];
+  public chosenSeason: string;
+  public chosenGender: string;
 
   private pageIndex: number = 0;
   private pageSize: number = 10;
@@ -36,34 +37,34 @@ export class ArticlesListComponent implements OnInit {
     public dialog: MatDialog,
     public articleService: ArticleService) {
 
-      this.store.select(state => state.articleState.allArticles)
+    this.store.select(state => state.articleState.allArticles)
       .subscribe(data => {
         this.articlesList = data.content;
         this.articlesLength = data.totalElements;
       });
 
-      this.store.select(state => state.articleState.brands)
+    this.store.select(state => state.articleState.brands)
       .subscribe(data => {
         this.brands = data;
       })
 
-      this.store.select(state => state.articleState.colors)
+    this.store.select(state => state.articleState.colors)
       .subscribe(data => {
         this.colors = data;
       })
 
-      this.store.select(state => state.articleState.sizes)
+    this.store.select(state => state.articleState.sizes)
       .subscribe(data => {
         this.sizes = data;
       })
 
-      this.store.select(state => state.articleState.categories)
+    this.store.select(state => state.articleState.categories)
       .subscribe(data => {
         this.categories = data;
       })
 
-      
-      
+
+
 
   }
 
@@ -72,24 +73,81 @@ export class ArticlesListComponent implements OnInit {
     this.articleService.getArticles(this.pageIndex, this.pageSize);
   }
 
-  filter(){
-    console.log('FILTER!!!');
-    this.articleService.filterArticles(this.pageIndex, this.pageSize, {
-      chosenSizes: this.chosenSizes, 
-      chosenColors: this.chosenColors, 
-      chosenCategories: this.chosenCategories, 
-      chosenBrands: this.chosenBrands, 
-      chosenStatuses: this.chosenStatuses});
+  onFilterChange(type: string) {
+    switch (type) {
+      case 'size':
+        for (let size of this.sizes) {
+          if (this.forbiddenSizes.indexOf(size.name) == -1) {
+            this.forbiddenSizes.push(size.name);
+          } else {
+            this.forbiddenSizes.splice(this.forbiddenSizes.indexOf(size.name), 1);
+          }
+        }
+        break;
+      case 'color':
+        for (let color of this.colors) {
+          if (this.forbiddenColors.indexOf(color.name) == -1) {
+            this.forbiddenColors.push(color.name);
+          } else {
+            this.forbiddenColors.splice(this.forbiddenColors.indexOf(color.name), 1);
+          }
+        }
+        break;
+      case 'category':
+        for (let cat of this.categories) {
+          if (this.forbiddenCategories.indexOf(cat.id) == -1) {
+            this.forbiddenCategories.push(cat.id);
+          } else {
+            this.forbiddenCategories.splice(this.forbiddenCategories.indexOf(cat.id), 1);
+          }
+        }
+        break;
+      case 'brand':
+        for (let brand of this.brands) {
+          if (this.forbiddenBrands.indexOf(brand.name) == -1) {
+            this.forbiddenBrands.push(brand.name);
+          } else {
+            this.forbiddenBrands.splice(this.forbiddenBrands.indexOf(brand.name), 1);
+          }
+        }
+        break;
+      case 'status':
+        for (let status of this.forbiddenStatuses) {
+          if (this.forbiddenStatuses.indexOf(status) == -1) {
+            this.forbiddenStatuses.push(status);
+          } else {
+            this.forbiddenStatuses.splice(this.forbiddenStatuses.indexOf(status), 1);
+          }
+        }
+        break;
+      default:
+        break;
+    }
+
+    let bindingModel = {
+      forbiddenSizes: this.forbiddenSizes,
+      forbiddenColors: this.forbiddenColors,
+      forbiddenCategories: this.forbiddenCategories,
+      forbiddenBrands: this.forbiddenBrands,
+      forbiddenStatuses: this.forbiddenStatuses,
+      chosenSeason: 'FALL_WINTER',
+      chosenGender: 'GIRLS'
+    };
+
+    console.log(bindingModel);
+
+    //this.articleService.filterArticles(this.pageIndex, this.pageSize, bindingModel);
+
   }
 
-  fetchRecords(message:any):void {
+  fetchRecords(message: any): void {
     this.pageIndex = message.pageIndex + 1;
     this.pageSize = message.pageSize;
 
     this.articleService.getArticles(this.pageIndex, this.pageSize);
   }
 
-  viewDetails(){
+  viewDetails() {
     this.dialog.open(ArticleDetailsComponent, {
       // data: {
       //   animal: 'panda'
